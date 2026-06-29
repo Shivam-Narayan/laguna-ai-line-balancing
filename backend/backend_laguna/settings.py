7b5a55cc4d8df400006c03c66cd08a453cb6257a
Shivam-Narayan <sshivam6495@gmail.com>
2026-06-29 17:17:22 +0530

    feat: initialize project settings with environment-based configurations

--- backend/backend_laguna/settings.py
+++ backend/backend_laguna/settings.py
@@ -73,7 +73,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'backend_laguna.custom_middleware.RequestFilterMiddleware',
 ]
 
 # CORS settings
